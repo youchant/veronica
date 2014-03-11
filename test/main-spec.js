2580b2cb6e8b46e25c6395274e3473d800a68afc
channing <guochant@gmail.com>
2014-03-11 19:06:03 +0800

    add unit test for browser

--- test/main-spec.js
+++ test/main-spec.js
@@ -3,6 +3,7 @@ define(['chai', 'veronica'], function (chai, core) {
     var should = chai.should();
 
     describe('core', function () {
+
         describe('util', function () {
             describe('#decamelize', function () {
                 it('should work normal string', function () {
@@ -34,12 +35,11 @@ define(['chai', 'veronica'], function (chai, core) {
             });
             describe('#include', function () {
                 it('extend constructor', function () {
-                    var A = function(){
-
+                    var A = function () {
                     };
-
-                    var r = core.util.include(A, { say: function(){ } });
-                    r.should.have.property('c').with.equal('3');
+                    var r = core.util.include(A, { say: function () {
+                    } });
+                    r.should.have.property('say').with.be.a('function');
                 });
                 it('same property', function () {
                     var obj = {a: '1', b: '2'};
@@ -49,6 +49,62 @@ define(['chai', 'veronica'], function (chai, core) {
                 });
             });
         });
+
+        describe('loader', function () {
+            describe('#loadWiget', function () {
+                it('base usage', function (done) {
+                    core.loadWiget('widget1', {}).done(function () {
+                        done();
+                    });
+                });
+                it('with wrong page', function (done) {
+                    core.createApp('test');
+                    core.loadWiget('widget1', {}, 'test-page').done(function () {
+
+                    }).fail(function () {
+                            done();
+                        });
+                });
+            });
+            describe('#start', function () {
+                it('load widget array', function (done) {
+                    core.start([
+                            {name: 'widget1', options: {}},
+                            {name: 'widget2'}
+                        ]).done(function () {
+                            done();
+                        });
+                });
+                it('load with callback', function (done) {
+                    core.start([
+                        {name: 'widget1'},
+                        {name: 'widget2'}
+                    ], function () {
+                        done();
+                    });
+                })
+            });
+            describe('#stop', function () {
+            });
+            describe('#stopBySandbox', function () {
+                it('pure object', function () {
+                    core.stopBySandbox({});
+                })
+                it('no paramas', function(){
+                    core.stopBySandbox();
+                })
+            });
+            describe('#stopByName', function () {
+            });
+            describe('#recycle', function () {
+            });
+            describe('#waitWidgets', function () {
+            });
+            describe('#registerWidgets', function () {
+            });
+        });
+
+
     });
 
 });
