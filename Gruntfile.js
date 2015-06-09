32738b6944084e5eaf50ee5d62dcae190b7cdcb3
channing <guochant@gmail.com>
2015-06-09 18:21:19 +0800

    add document comment

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function (grunt) {
                         'art-dialog': '../assets/artDialog/dist/dialog-plus',
                         'text': '../bower_components/requirejs-text/text'
                     },
-                    'shim':{
+                    'shim': {
                         'art-dialog': { 'exports': 'dialog', deps: ['jquery'] },
                         'noty': { 'exports': 'noty' }
                     },
@@ -45,13 +45,26 @@ module.exports = function (grunt) {
                 },
                 report: 'gzip'
             }
+        },
+        jsdoc: {
+            dist: {
+                src: ['lib/**/*.js', 'README.md'],
+                options: {
+                    verbose: true,
+                    destination: './docs',
+                    configure: 'jsdoc-conf.json',
+                    template: 'assets/jaguarjs-jsdoc',
+                    'private': false
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-requirejs');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-jsdoc');
 
     grunt.registerTask('default', ['requirejs', 'clean', 'uglify']);
-
+    grunt.registerTask('doc', ['jsdoc']);
 };
